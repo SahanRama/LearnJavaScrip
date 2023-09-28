0f0db5ad6859519bd171f2139f20ccaa99c58d43
Sahan Ramanayake <sahan@pastbook.com>
2023-09-28 09:42:09 +0530

    first fundamental parts
    first fundamental parts

---
+++ 01-Fundamentals-Part-1/assignments/script.js
@@ -0,0 +1,15 @@
+//assignment
+const coutry = "Sri Lanka";
+let continent = "Colombo";
+let population = "1.3M";
+
+let isIsland = true;
+let language;
+
+console.log(coutry);
+console.log(continent);
+console.log(population);
+console.log(language);
+
+language = "Sinhala";
+console.log(language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,53 @@
+/*
+let js = "amazing";
+
+console.log(80 + 70 + 12 - 10);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let coutry = "Sri Lanka";
+let continent = "Colombo";
+let population = "1.3M";
+
+
+// data types
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Sahan");
+
+console.log(typeof null);
+
+
+//assignment
+
+let isIsland = true;
+let language;
+
+console.log(coutry);
+console.log(continent);
+console.log(population);
+console.log(language);
+
+
+// let, const and var
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1993;
+// birthYear = 1992;
+// const job;
+*/
+
+const ageSahan = 2037 - 1993;
+const ageKaush = 2037 - 1995;
+console.log(ageSahan, ageKaush);
